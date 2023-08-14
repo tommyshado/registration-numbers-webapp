0404027534de9186686bbbe32425699d0dda4271
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-08-14 10:05:12 +0200

    created a basic layout, added basic styling, configured the handlebars engine, body-parsers, rendered the "index" route

--- index.js
+++ index.js
@@ -6,11 +6,27 @@ import bodyParser from "body-parser";
 // instances
 const app = express();
 
+app.use(express.static("public"));
+
+const handlebarSetup = exphbs.engine({
+    partialsDir: "./views/partials",
+    viewPath: "./views",
+    layoutsDir: "./views/layouts",
+});
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+app.engine("handlebars", handlebarSetup);
+app.set("view engine", "handlebars");
+app.set("views", "./views");
 
 // ROUTES;
 
 app.get("/", (req, res) => {
-    res.send("Registration Numbers Application");
+    res.render("index");
 });
 
 const PORT = process.env.PORT || 3007;
