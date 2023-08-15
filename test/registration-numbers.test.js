59c149854fe539e7974b5733f92166348531ab2c
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-08-15 09:39:34 +0200

    removed test cases for the customized registration number

--- test/registration-numbers.test.js
+++ test/registration-numbers.test.js
@@ -20,25 +20,12 @@ describe("registrationApp", () => {
         assert.deepStrictEqual(["CA 662"], RegNumbersApp.getRegNumbers());
     });
 
-    it("should be able to set and get a valid customized registration number", () => {
-        RegNumbersApp.setRegNumber("jolaksi CA");
-
-        assert.deepStrictEqual(["JOLAKSI CA"], RegNumbersApp.getRegNumbers());
-    });
-
     it("should be able to the same registration number twice and get one", () => {
         // set registration number
         RegNumbersApp.setRegNumber("cj 552-232");
         RegNumbersApp.setRegNumber("cJ 552-232");
 
         assert.deepStrictEqual(["CJ 552-232"], RegNumbersApp.getRegNumbers());
-    });
-
-    it("should be able to the same customized number twice and get one", () => {
-        RegNumbersApp.setRegNumber("jolaksi CA");
-        RegNumbersApp.setRegNumber("Jolaksi CA");
-
-        assert.deepStrictEqual(["JOLAKSI CA"], RegNumbersApp.getRegNumbers());
     });
 
     // filtering registration numbers
@@ -61,17 +48,6 @@ describe("registrationApp", () => {
         assert.deepStrictEqual(["CJ 223"], RegNumbersApp.getRegNumbers());
     });
 
-    // it("should be able to filter for customized registration number", () => {
-    //     RegNumbersApp.setRegNumber("jolaksi ca");
-    //     RegNumbersApp.setRegNumber("cl 553");
-        
-    //     RegNumbersApp.setTownOrCustomRegNumber("Custom");
-
-    //     console.log(RegNumbersApp.getRegNumbers());
-
-    //     assert.deepStrictEqual(["JOLAKSI CA"], RegNumbersApp.getRegNumbers());
-    // });
-
     it("should return an empty array when filtering for town that contains no registration numbers", () => {
         RegNumbersApp.setRegNumber("ca 662");
         RegNumbersApp.setRegNumber("cl 553");
@@ -89,23 +65,10 @@ describe("registrationApp", () => {
         assert.equal("Please enter a registration number.", RegNumbersApp.getMessages().errorMessage);
     });
 
-    it("should be able to return 'Please enter a valid registration number. eg. CA 563-464, CJ 536, CL 7733, Jolaksi CA, Tolokazi CJ.'", () => {
+    it("should be able to return 'Please enter a valid registration number. eg. CA 563-464, CJ 536, CL 7733.'", () => {
         RegNumbersApp.setRegNumber("12453");
 
-        assert.equal("Please enter a valid registration number. eg. CA 563-464, CJ 536, CL 7733, Jolaksi CA, Tolokazi CJ.", RegNumbersApp.getMessages().errorMessage);
-    });
-
-    it("should be able to return 'Successfully added a registration number.'", () => {
-        RegNumbersApp.setRegNumber("Jolaski CA");
-
-        assert.equal("Successfully added a registration number.", RegNumbersApp.getMessages().successMessage);
-    });
-
-    it("should be able to return 'TOLO CL has already been entered.'", () => {
-        RegNumbersApp.setRegNumber("Tolo CL");
-        RegNumbersApp.setRegNumber("Tolo CL");
-
-        assert.equal("TOLO CL has already been entered.", RegNumbersApp.getMessages().errorMessage);
+        assert.equal("Please enter a valid registration number. eg. CA 563-464, CJ 536, CL 7733.", RegNumbersApp.getMessages().errorMessage);
     });
 
     // alert classnames from bootstrap
