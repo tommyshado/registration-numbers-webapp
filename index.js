80b2d0a308c241f378f0e9d77a0ab1651d7e13c3
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-08-17 10:12:51 +0200

    added session for express-session

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import routes from "./routes/routes.js";
 import pgPromise from "pg-promise";
 import "dotenv/config";
 import flash from "express-flash";
-// import session from "express-session";
+import session from "express-session";
 
 // instances
 const app = express();
