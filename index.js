c088aaabd5c1f5ee9e40b8086e341bda1a085797
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-08-17 10:03:41 +0200

    removed session cookie

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ import pgPromise from "pg-promise";
 import "dotenv/config";
 import flash from "express-flash";
 // import session from "express-session";
-import session from "cookie-session";
 
 // instances
 const app = express();
